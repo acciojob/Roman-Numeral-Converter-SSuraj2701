c18cc0a26878f8193bb45c651de846d3ee7db867
Suraj Singh <suraj.job.2002@gmail.com>
2025-11-11 17:01:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  	const obj = [
+        ['M',1000], 
+        ['D', 500], 
+        ['C', 100], 
+        ['L', 50], 
+	    ['X', 10], 
+	    ['V', 5], 
+	    ['I', 1]
+    ];
 
   //your code here
   let result = '';
-  for (let [symb, val] of Object.entries(obj)) {
+  for (let [symb, val] of  obj) {
     while (num >= val) {
       result = result + symb;
       num = num - val;
