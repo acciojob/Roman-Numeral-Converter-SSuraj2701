e9c012d1803432a155323a29f0150772b15c57e9
Suraj Singh <suraj.job.2002@gmail.com>
2025-11-11 16:51:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ function convertToRoman(num) {
 			num=num-val;
 		}
 	}
-
+	return result;
+ 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
